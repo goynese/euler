9756f22505a683ca69141dc2fe7c8c192e4d61e1
bob <yoshi@localhost.localdomain>
2015-08-15 22:44:04 -0700

    Reorganized

---
+++ P10 Summation of primes/python/p10.py
@@ -0,0 +1,19 @@
+import sys
+#Sums prime less than n.
+
+def sum_of_primes(n):
+	print ("hello")
+
+
+
+
+#sieve of Eratosthenes
+
+def sieve_of_Eratosthenes(n):
+	numbers = [range(n)]
+
+	for n in numbers:
+		print (n)
+
+if __name__ == '__main__':
+    sieve_of_Eratosthenes(10)
