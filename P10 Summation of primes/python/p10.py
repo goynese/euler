843a347428c70f80ac90efc75f4921c047b4dae4
Eddie Goynes <eddiegoynes@yahoo.com>
2015-08-16 11:00:29 -0700

    Python implementation

--- P10 Summation of primes/python/p10.py
+++ P10 Summation of primes/python/p10.py
@@ -1,21 +1,41 @@
-import sys
+import math, sys
 #Sums prime less than n.
 
 def sum_of_primes(n):
-	print ("hello")
+	primes = sieve_of_Eratosthenes(n)
+	psum = 0
+	for n in primes:
+		psum += n
 
-
+	print psum
 
 
 #sieve of Eratosthenes
 
-def sieve_of_Eratosthenes(n):
-	numbers = [range(n)]
+def sieve_of_Eratosthenes(limit):
+	nums = list(range(limit+1))
+	sqtlim = math.sqrt(limit)
+	nums[1] = 0 #make 1 zero
 
-	for n in numbers:
-		print (n)
+	for n in nums[2:int(sqtlim)+1]:
+		if n != 0:
+			i = 2
+
+			while n*i <= limit:
+				nums[n*i] = 0
+				i += 1
+
+    #Create a list of just primes, filter out the zeros
+	primes = []
+	for n in nums:
+		if n != 0:
+			primes.append(n)
+
+	return primes
+
+
 
 if __name__ == '__main__':
-    sieve_of_Eratosthenes(10)
-    #commit here
+    sum_of_primes(int(sys.argv[1]))
+   
     
