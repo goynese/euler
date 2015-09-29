fd105733cb0bec2539b5df35f5a0edc3df729dc8
Eddie Goynes <eddiegoynes@yahoo.com>
2015-09-28 23:42:59 -0700

    Max product in a grid

--- P11 Largest product in a grid/python/p11.py
+++ P11 Largest product in a grid/python/p11.py
@@ -8,11 +8,41 @@ def create_matrix():
 
 	return matrix
 
+def m4(matrix, x1, y1, x2, y2):
+	 if x2 < 0 or y2 < 0 or x2 > 19 or y2 > 19:
+	 	return 0
+	 num = 1
+	 for x in range(min(x1, x2), max(x1, x2)+1):
+	 	for y in range(min(y1, y2), max(y1, y2)+1):
+	 		num *= matrix[x][y]
+	 return num
+
+
+
+def mult4(matrix, x, y):
+	# Assumption I only have positive x and y
+	return max(
+		m4(matrix, x, y, x, y+4),
+		m4(matrix, x, y, x+4, y+4),
+		m4(matrix, x, y, x+4, y),
+		m4(matrix, x, y, x+4, y-4),
+		m4(matrix, x, y, x, y-4),
+		m4(matrix, x, y, x-4, y-4),
+		m4(matrix, x, y, x-4, y),
+		m4(matrix, x, y, x-4, y+4))
+
+
 def max4Product(matrix):
-		
+	# Goes through all the numbers
+	maxprod = 0
+	for y, lis in enumerate(matrix):
+		for x, val in enumerate(lis):
+			maxprod = max(maxprod, mult4(matrix, x,y))
+	return maxprod
 
 if __name__ == '__main__':
 	matrix = create_matrix()
-	print (matrix)
-
+	# print (matrix)
+	print(max4Product(matrix))
+	
 
